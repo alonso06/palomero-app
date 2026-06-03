8831dd4ffb2a4e5053e03f8cadc4873db821992f
aln <alonsochiroque03@gmail.com>
2026-06-03 03:58:20 -0500

    feat(Carrousel): Carrousel design

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-
+import { Header } from "./components/Header";
 
 function App() {
-
-
   return (
     <>
-      <div>Cinefilo app</div>
+      <Header />
+      <section className="px-5">
+        <div className="">
+          <p>Lo más valorado</p>
+          <h1>RAYA AND THE LAST DRAGON</h1>
+          <p>Las historia de raya jamas contada</p>
+          <picture>
+            <img src="" alt="" />
+          </picture>
+        </div>
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+export const Header = () => {
+  return (
+    <>
+      <div className="p-2.5 bg-amber-300 text-center">
+        <a href="">CinefiloApp</a>
+      </div>
+    </>
+  );
+};
