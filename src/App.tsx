270ffd93cd7762760f4ee35f440b04e226cfd57c
aln <alonsochiroque03@gmail.com>
2026-06-10 20:06:59 -0500

    refactor(Header): refactor header
    create component searchBar

--- src/App.tsx
+++ src/App.tsx
@@ -4,97 +4,101 @@ import { getTrendingMoviesByDay } from "./movies/actions/get-trending-movies-by-
 import type { Movie } from "./movies/interfaces/movie.interface";
 import { Carrousel } from "./movies/components/Carrousel";
 import { CardFavoriteMovie } from "./movies/components/CardFavoriteMovie";
+const mockDataSearchMovies = [
+  {
+    id: 1,
+    title: "MOVIE 01",
+    url: "https://picsum.photos/seed/picsum/200/300",
+    overview:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit.Excepturi quas rem molestiae",
+    release_date: "26/06/12",
+    adult: true,
+  },
+  {
+    id: 2,
+    title: "MOVIE 02",
+    url: "https://picsum.photos/seed/picsum/200/300",
+    overview:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit.Excepturi quas rem molestiae",
+    release_date: "26/06/12",
+    adult: true,
+  },
+  {
+    id: 3,
+    title: "MOVIE 03",
+    url: "https://picsum.photos/seed/picsum/200/300",
+    overview:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit.Excepturi quas rem molestiae",
+    release_date: "26/06/12",
+    adult: true,
+  },
+  {
+    id: 4,
+    title:
+      "MOVIE 03 MOSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS",
+    url: "https://picsum.photos/seed/picsum/200/300",
+    overview:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit.Excepturi quas rem molestiae",
+    release_date: "26/06/12",
+    adult: true,
+  },
+  {
+    id: 5,
+    title: "MOVIE 03",
+    url: "https://picsum.photos/seed/picsum/200/300",
+    overview:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit.Excepturi quas rem molestiae",
+    release_date: "26/06/12",
+    adult: true,
+  },
+  {
+    id: 6,
+    title: "MOVIE 03",
+    url: "https://picsum.photos/seed/picsum/200/300",
+    overview:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit.Excepturi quas rem molestiae",
+    release_date: "26/06/12",
+    adult: true,
+  },
+  {
+    id: 7,
+    title: "MOVIE 03",
+    url: "https://picsum.photos/seed/picsum/200/300",
+    overview:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit.Excepturi quas rem molestiae",
+    release_date: "26/06/12",
+    adult: true,
+  },
+];
+
+//TODO: Revisar manejo de excepción
+const sortMoviesByPopularity = (movies: Movie[]) => {
+  return [...movies].sort((a, b) => {
+    b.popularity - a.popularity;
+  });
+};
+
+const getTrendingMovies = async () => {
+  const movies = await getTrendingMoviesByDay();
+  return sortMoviesByPopularity(movies);
+};
 
 function App() {
   const [trendingMovies, setTrendingMovies] = useState<Movie[]>([]);
 
-  const sortMoviesByPopularity = (movies: Movie[]) => {
-    const sortedMovies = [...movies].sort(
-      (a, b) => b.popularity - a.popularity,
-    );
-    setTrendingMovies(sortedMovies);
+  const loadMovies = async () => {
+    const movies = await getTrendingMovies();
+    setTrendingMovies(movies);
   };
-
-  const getTrendingMovies = async () => {
-    const movies = await getTrendingMoviesByDay();
-    sortMoviesByPopularity(movies);
-    // setTrendingMovies(movies);
-  };
-
-  const mockDataSearchMovies = [
-    {
-      id: 1,
-      title: "MOVIE 01",
-      url: "https://picsum.photos/seed/picsum/200/300",
-      overview:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit.Excepturi quas rem molestiae",
-      release_date: "26/06/12",
-      adult: true,
-    },
-    {
-      id: 2,
-      title: "MOVIE 02",
-      url: "https://picsum.photos/seed/picsum/200/300",
-      overview:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit.Excepturi quas rem molestiae",
-      release_date: "26/06/12",
-      adult: true,
-    },
-    {
-      id: 3,
-      title: "MOVIE 03",
-      url: "https://picsum.photos/seed/picsum/200/300",
-      overview:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit.Excepturi quas rem molestiae",
-      release_date: "26/06/12",
-      adult: true,
-    },
-    {
-      id: 4,
-      title:
-        "MOVIE 03 MOSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS",
-      url: "https://picsum.photos/seed/picsum/200/300",
-      overview:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit.Excepturi quas rem molestiae",
-      release_date: "26/06/12",
-      adult: true,
-    },
-    {
-      id: 5,
-      title: "MOVIE 03",
-      url: "https://picsum.photos/seed/picsum/200/300",
-      overview:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit.Excepturi quas rem molestiae",
-      release_date: "26/06/12",
-      adult: true,
-    },
-    {
-      id: 6,
-      title: "MOVIE 03",
-      url: "https://picsum.photos/seed/picsum/200/300",
-      overview:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit.Excepturi quas rem molestiae",
-      release_date: "26/06/12",
-      adult: true,
-    },
-    {
-      id: 7,
-      title: "MOVIE 03",
-      url: "https://picsum.photos/seed/picsum/200/300",
-      overview:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit.Excepturi quas rem molestiae",
-      release_date: "26/06/12",
-      adult: true,
-    },
-  ];
 
   useEffect(() => {
-    getTrendingMovies();
+    loadMovies();
   }, []);
 
   return (
     <>
       <Header />
+      {/* Hero section */}
       <section className="w-full h-[70vh] text-black flex flex-col justify-center items-center">
         {/* SearchMovies */}
         <Carrousel movies={trendingMovies}></Carrousel>
--- src/shared/components/Header.tsx
+++ src/shared/components/Header.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef, useState } from "react";
+import { SearchBar } from "./SearchBar";
 
 export const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -38,47 +39,11 @@ export const Header = () => {
         >
           CINEFILO
         </a>
-        <div
-          className={`z-20 absolute inset-x-0 flex items-center justify-center px-14 transition-all duration-300 ${isOpen ? "opacity-100 translate-y-0 pointer-events-auto" : "opacity-0 -translate-y-2 pointer-events-none"} `}
-        >
-          <div className="flex items-center gap-3 w-full max-w-lg bg-white/10 hover:bg-white/5 backdrop-blur-xl border border-white/10 rounded-full px-5 py-3">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              className="w-5 h-5 icon icon-tabler icons-tabler-outline icon-tabler-search fill-transparent stroke-white/50 shrink-0"
-            >
-              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-              <path d="M3 10a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
-              <path d="M21 21l-6 -6" />
-            </svg>
-            <input
-              ref={inputRef}
-              type="text"
-              placeholder="Buscar película"
-              className="flex-1 bg-transparent border-none outline-none text-white text-lg placeholder:text-white/50 caret-white"
-            />
-            <button
-              onClick={() => setIsOpen(false)}
-              className="text-white/80 hover:text-white transition-colors"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                className="w-6 h-6 icon icon-tabler icons-tabler-outline icon-tabler-square-rounded-x fill-transparent stroke-white"
-              >
-                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <path d="M10 10l4 4m0 -4l-4 4" />
-                <path d="M12 3c7.2 0 9 1.8 9 9c0 7.2 -1.8 9 -9 9c-7.2 0 -9 -1.8 -9 -9c0 -7.2 1.8 -9 9 -9" />
-              </svg>
-            </button>
-          </div>
-        </div>
+        <SearchBar
+          isOpen={isOpen}
+          inputRef={inputRef}
+          setIsOpen={setIsOpen}
+        ></SearchBar>
         <div>
           <button
             onClick={() => setIsOpen(true)}
