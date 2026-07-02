2f76db636e4a79a5864c0789fd3a109c707832d8
aln <alonsochiroque03@gmail.com>
2026-07-01 23:05:25 -0500

    feat(Favorite movies): update handle add and delete favorites

--- src/movies/components/CardMovie.tsx
+++ src/movies/components/CardMovie.tsx
@@ -19,68 +19,83 @@ export const CardMovie = (props: Props) => {
   const { mode, movie } = props;
   const { title, url } = movie;
 
-  const [confirmingDelete, setConfirmingDelete] = useState(false);
+  const [showOverlay, setShowOverlay] = useState(false);
 
-  const isSelected = mode === "search" && props.idFavoriteMovies.has(movie.id);
+  const isSelected =
+    mode === "search" ? props.idFavoriteMovies.has(movie.id) : false;
 
-  const handleClick = () => {
+  const handleOverlayClick = () => {
+    setShowOverlay(!showOverlay);
+  };
+
+  const handleDelete = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    props.onDeleteFavorite(movie);
+    setShowOverlay(false);
+  };
+
+  const handleAdd = (e: React.MouseEvent) => {
+    e.stopPropagation();
     if (mode === "search") {
-      if (isSelected) {
-        props.onDeleteFavorite(movie);
-        console.log("Eliminar");
-      } else {
-        props.onAddToFavorites(movie);
-      }
-    } else {
-      setConfirmingDelete(true);
+      props.onAddToFavorites(movie);
     }
+    setShowOverlay(false);
   };
-
-  const handleConfirmDelete = (e: React.MouseEvent) => {
-    console.log("Render en confirmDelete");
-    e.stopPropagation();
-    if (mode === "favorites") {
-      props.onDeleteFavorite(movie);
-    }
-  };
-
-  const handleCancelDelete = (e: React.MouseEvent) => {
-    e.stopPropagation();
-    setConfirmingDelete(false);
-  };
-
-  const isDimmed = mode === "search" ? isSelected : confirmingDelete;
 
   return (
     <>
       <div className="relative w-full h-full">
-        {/* Desde favoritos */}
-        {mode === "favorites" && confirmingDelete && (
-          <div className="absolute w-full h-full inset-x-0 z-10 flex flex-col justify-center items-center gap-2 bg-black/70 px-2 py-1">
-            <span className="text-white text-lg font-bold">
-              ¿Quitar de la lista?
-            </span>
-            <div className="flex gap-4">
-              <button
-                onClick={handleConfirmDelete}
-                className="bg-red-500 px-6 py-1 text-white rounded-full text-lg font-bold cursor-pointer hover:bg-red-600"
-              >
-                Sí
-              </button>
-              <button
-                onClick={handleCancelDelete}
-                className="bg-gray-200 px-6 py-1 text-black rounded-full text-lg font-bold cursor-pointer hover:bg-gray-300"
-              >
-                No
-              </button>
+        {showOverlay && (
+          <div
+            className="absolute w-full h-full inset-x-0 z-10 flex flex-col justify-center items-center gap-2 bg-black/50 px-2 py-1"
+            onClick={handleOverlayClick}
+          >
+            <div className="mx-auto">
+              {mode === "search" && !isSelected ? (
+                <button
+                  onClick={handleAdd}
+                  className="bg-green-600 px-3 py-3 rounded-full cursor-pointer hover:bg-green-700"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    className="w-7 h-7 stroke-white fill-none icon icon-tabler icons-tabler-outline icon-tabler-heart hover:fill-white"
+                  >
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" />
+                  </svg>
+                </button>
+              ) : (
+                <button
+                  onClick={handleDelete}
+                  className="bg-red-600 px-3 py-3 rounded-full cursor-pointer hover:bg-red-700"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    className="w-7 h-7 stroke-white fill-none icon icon-tabler icons-tabler-outline icon-tabler-heart-off hover:fill-white"
+                  >
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M3 3l18 18" />
+                    <path d="M19.5 12.572l-1.5 1.428m-2 2l-4 4l-7.5 -7.428a5 5 0 0 1 -1.288 -5.068a4.976 4.976 0 0 1 1.788 -2.504m3 -1c1.56 0 3.05 .727 4 2a5 5 0 1 1 7.5 6.572" />
+                  </svg>
+                </button>
+              )}
             </div>
           </div>
         )}
+        {/* )} */}
         <div
-          onClick={handleClick}
+          onClick={handleOverlayClick}
           key={movie.id}
           data-slot="card"
-          className={`w-full h-full transition-opacity duration-300 ${isDimmed ? "opacity-40" : "opacity-100"}`}
+          className={`w-full h-full transition-opacity duration-300 ${isSelected ? "opacity-40" : "opacity-100"}`}
         >
           <div className="relative w-full h-full">
             <picture
